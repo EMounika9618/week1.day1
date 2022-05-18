d5a459bb25607ed792f33c7646e0cb690a552c86
mounika <105705466+EMounika9618@users.noreply.github.com>
2022-05-18 12:20:08 +0530

    factorial

---
+++ Functionmain.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Functionmain {
+
+	public Functionmain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int input=5;
+		int fact=1;
+		for (int i=1;i<=input ;i++)
+		{
+		fact=(fact*i);	
+		}
+		System.out.println("factorial of 5 is: "+fact);
+	}
+	}
+
