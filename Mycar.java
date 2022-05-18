431f68fc300653787aad041c9eee0549cceb336d
mounika <105705466+EMounika9618@users.noreply.github.com>
2022-05-18 12:20:53 +0530

    Mycar

---
+++ Mycar.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Mycar {
+	public void applyBreak() {
+		System.out.println("Inside Apply Break method");
+	}
+	public void applyGear() {
+		System.out.println("Inside Apply Gear method");
+	}
+	public void switchonAc() {
+		System.out.println("Inside Switch on AC method");
+	}
+	public void applyAcclerate() {
+		System.out.println("Inside Apply Accelerate method");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mycar obj=new Mycar();
+		obj.applyBreak();
+		obj.applyGear();
+		obj.switchonAc();
+		obj.applyAcclerate();
+	}
+	}
+
+
