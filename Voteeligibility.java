a89e5436942406ec5d788b41cbe50f24a29f0b5a
mounika <105705466+EMounika9618@users.noreply.github.com>
2022-05-19 18:34:50 +0530

    vote eligibility

---
+++ Voteeligibility.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Voteeligibility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 9;
+		if(age >= 18)
+		{
+			System.out.println("Eligible for Voting");
+		}
+		else
+		{
+			System.out.println("not Eligible for Voting");
+		}
+		}
+	}
+
+
